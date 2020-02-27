53ae585b21ad87d096e0a318f35da57b7e52e39f
chiyubitian <953369684@qq.com>
2020-02-27 16:55:42 +0800

    delect

--- dumplingApi/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-  applet:{
-    AppId:'wx4bf2153174472fbd',
-    AppSecret:'25c6b9d07782f978d5ca25017cb64176'
-  }
-}
--- wx_shop/utils/base.js
+++
@@ -1,8 +0,0 @@
-import Axios from "./axiso.js"
-class Base extends Axios{
-    constructor(){
-        super()
-        this.baseurl = "http://127.0.0.1:3004"
-    }
-}
-export default Base
